43a5d4e11f5f50f750c15304bb693c7e6f67f88d
achahine78 <achahine78@gmail.com>
2023-05-14 15:54:24 +0400

    feat: create basic UI for login and signup pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/login/LoginPage';
+import SignupPage from './pages/signup/SignupPage';
+
 function App() {
   return (
-    <div>Hello world!</div>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+    </Routes>
   )
 }
 
---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,50 @@
+import { Input, Button, Form, Card } from "antd";
+
+const onFinish = (values: any) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+  console.log("Failed:", errorInfo);
+};
+
+const LoginPage = () => {
+  return (
+    <Card
+      title="Login"
+      style={{ width: 300 }}
+    >
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
+};
+
+export default LoginPage;
