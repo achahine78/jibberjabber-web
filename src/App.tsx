453e95801876c9efcb42bb8ce73d993aea7ba1b0
achahine78 <achahine78@gmail.com>
2023-05-28 21:37:23 +0400

    feat: implement auth guard and guard /home

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
-import { Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/login/LoginPage';
-import SignupPage from './pages/signup/SignupPage';
-import HomePage from './pages/home/HomePage';
-import AppShell from './components/appshell/AppShell';
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/login/LoginPage";
+import SignupPage from "./pages/signup/SignupPage";
+import HomePage from "./pages/home/HomePage";
+import AppShell from "./components/appshell/AppShell";
+import AuthGuard from "./guards/AuthGuard";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
-      <Route path="/home" element={
-        <AppShell>
-          <HomePage />
-        </AppShell>
-      } />
+        <Route
+          path="/home"
+          element={
+            <AuthGuard>
+              <AppShell>
+                <HomePage />
+              </AppShell>
+            </AuthGuard>
+          }
+        />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
